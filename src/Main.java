24a339fdf0564654c4f0b3bcdb6e25fb1a0ae067
Glenn Li <34758781+glennli-Code@users.noreply.github.com>
2023-09-11 16:05:27 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         FizzBuzz fizzBuzz = new FizzBuzz();
         fizzBuzz.main(args);
+        // little change to test for git pull
     }
 }
